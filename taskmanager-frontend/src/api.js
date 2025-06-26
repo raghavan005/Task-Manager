3434da45fd9fce052199e44d9ef66e63a0cf1219
unknown <rragavan2004@gmail.com>
2025-06-26 14:11:13 +0530

    api change in js

--- taskmanager-frontend/src/api.js
+++ taskmanager-frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000"; 
+const API_BASE = "http://0.0.0.0:10000"; 
 export const getAuthHeaders = () => {
   const token = localStorage.getItem("token");
   return {
